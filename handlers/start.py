67152653b667a3bb4317a6fbf9d79d5303cc0642
lantrik <79407681+lantrik@users.noreply.github.com>
2022-10-21 22:58:41 +0700

    v0.1.3b4

--- handlers/start.py
+++ handlers/start.py
@@ -1,10 +1,6 @@
-from src.core import Bot
-
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
-
-from src.ext.keyboards import problem_keyboard
 
 
 router = Router()
@@ -12,10 +8,3 @@ router = Router()
 @router.message(Command(commands=["start"]))
 async def start_command(message: Message) -> None:
     await message.answer("Привет!")
-
-    await message.answer("Для начала, можешь выбрать одну из тем, которые тебя беспокоят? 😣")
-
-    await message.answer(
-        "Главное — помни, что не только ты можешь столкнуться с трудностями, они бывают у всех.",
-        reply_markup=problem_keyboard()
-        )
